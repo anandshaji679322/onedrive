1019b13af0f8920bd42beb6d59bf7d79c32d25a5
Anand c <90331726+anandshaji679322@users.noreply.github.com>
2026-02-26 19:40:46 +0530

    Remove 'New folder (2)' from protectedRoutes

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Pvt','/doc','/New folder (2)',],
+  protectedRoutes: ['/Pvt','/doc',],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
